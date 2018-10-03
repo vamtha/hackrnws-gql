68529b9bf52a145ac8b1b97b8aedce83f4c84c9e
Jonathan Vazquez <vamtha24@gmail.com>
2018-10-03 00:28:31 -0500

    Implement AddLink Mutation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}];
+
+let idCount = links.length;
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+
+      links.push(link);
+      return link;
+    },
   }
 };
 
